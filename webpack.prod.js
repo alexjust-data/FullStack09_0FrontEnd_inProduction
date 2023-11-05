7f4b949c53f0a8f4c2516f00033a90275fa77ca0
Alex Just <alexjustdata@gmail.com>
2023-11-05 14:56:08 +0100

    Webpack 10: Eslint. Prod CSS Extract

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,7 @@
 const { merge } = require('webpack-merge'); 
 const defaultConfig = require('./webpack.common');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(defaultConfig, {
     mode: 'production', 
@@ -7,4 +9,27 @@ module.exports = merge(defaultConfig, {
     output:{
         clean: true,
     },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // 'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader', 
+                    'sass-loader'
+                ],
+            }
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[chunkhash].dundle.css',
+        })
+    ]
 });
